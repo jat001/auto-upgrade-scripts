1cdb58e7d118642deba2f3dd8ca5949d842dac1a
Jat <dpjat1@gmail.com>
2014-08-23 19:16:42 +0800

    c

--- version.php
+++ version.php
@@ -2,30 +2,30 @@
 
 switch ($_GET['pro']) {
     case 'php':
-        $html = file_get_contents('http://php.net/downloads.php');
+        $html = file_get_contents('http://php.net/downloads.php') or exit();
         preg_match('/Current Stable.+?PHP (\d\.\d{1,2}\.\d{1,3})/is', $html, $matches);
         exit($matches[1]);
 
         break;
 
     case 'mysql':
-        $html = file_get_contents('http://dev.mysql.com/downloads/mysql/');
+        $html = file_get_contents('http://dev.mysql.com/downloads/mysql/') or exit();
         preg_match('/MySQL Community Server (\d\.\d{1,2}\.\d{1,3})/i', $html, $matches);
         exit($matches[1]);
 
         break;
 
     case 'nginx':
-        $html = file_get_contents('http://nginx.org/en/download.html');
+        $html = file_get_contents('http://nginx.org/en/download.html') or exit();
         preg_match('/nginx-(\d\.\d{1,2}\.\d{1,3})/i', $html, $matches);
         exit($matches[1]);
 
         break;
 
     case 'phpmyadmin':
-        $html = file_get_contents('http://sourceforge.net/projects/phpmyadmin/files/');
-        preg_match('/Download phpMyAdmin-(\d\.\d{1,2}\.\d{1,3})-all-languages/i', $html, $matches);
-        exit($matches[1]);
+        $html = file_get_contents('http://www.phpmyadmin.net/home_page/version.json') or exit();
+        $version = json_decode($html, true);
+        exit($version['version']);
 
         break;
 
