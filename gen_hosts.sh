816abf154930d1c7e9910105d02bf735d5676e49
Jat <dpjat1@gmail.com>
2015-03-03 12:59:22 +0800

    fix a bug

--- gen_hosts.sh
+++ gen_hosts.sh
@@ -8,13 +8,13 @@ fi
 
 cd /data/script/google-hosts
 git reset --hard
-git pull -v
+git pull
 
 cd ./scripts
 env LC_TIME=en_US.UTF-8 ./auto.sh 216.239.32 64.233.160 66.249.80 72.14.192 209.85.128 66.102 74.125 64.18 207.126.144 173.194
 
 hosts=$(cat ../hosts)
-$redis setex 'hosts' 604800 "$hosts"
+$redis setex 'hosts' 604800 "s:${#hosts}:\"$hosts\";"
 
 rm -r ./output
 
